10361abd3fbeee12d111d02b3b63541ccfd24569
Farnaam Samadi <fm.samdi70@gmail.com>
2020-03-06 23:12:23 +0330

    Updates

---
+++ precache-manifest.ab67b5fd6c112fdcb888fa1620cd7f3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51dccb8404fd87510ecf99d469f6679d",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "f5ee286917ea6977b825",
+    "url": "/resume/static/css/main.2c6e3cbf.chunk.css"
+  },
+  {
+    "revision": "4143b0ab0e88376ffdf4",
+    "url": "/resume/static/js/2.baf0bac6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/resume/static/js/2.baf0bac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ee286917ea6977b825",
+    "url": "/resume/static/js/main.70302ebe.chunk.js"
+  },
+  {
+    "revision": "d463f5f8adb252236c27",
+    "url": "/resume/static/js/runtime-main.64038317.js"
+  }
+]);
