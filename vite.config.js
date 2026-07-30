0938358fdc499231716db60d14d091f9a59757bb
FarnaamSamadi <fm.samadi70@gmail.com>
2026-07-30 23:14:53 +0330

    update deps and fix vulnarabilities

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import UnoCSS from 'unocss/vite'
-
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@css': fileURLToPath(new URL('./src/assets/styles', import.meta.url)),
-      '@imgs': fileURLToPath(new URL('./src/assets/images', import.meta.url)),
-      '@cmps': fileURLToPath(new URL('./src/components', import.meta.url)),
-    },
-  },
-
-  plugins: [UnoCSS(), react()],
-})
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import UnoCSS from 'unocss/vite'
+
+
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@css': fileURLToPath(new URL('./src/assets/styles', import.meta.url)),
+      '@imgs': fileURLToPath(new URL('./src/assets/images', import.meta.url)),
+      '@cmps': fileURLToPath(new URL('./src/components', import.meta.url)),
+    },
+  },
+  plugins: [UnoCSS(), react()],
+})
+
